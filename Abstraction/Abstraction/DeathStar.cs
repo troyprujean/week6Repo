085a6c3d0013f99829541e42ecb4dbaed863d6b2
troy prujean <9930285@student.toiohomai.ac.nz>
2018-08-30 09:57:44 +1200

    abstraction

---
+++ Abstraction/Abstraction/DeathStar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public class DeathStar : SpaceStation
+    {
+        public override int HitPoints { get; set; }
+        
+        public override int Damage { get; set;  }
+
+        public override string FireLaser()
+        {
+            return "Pew pew\n";
+        }
+
+        public DeathStar (int hitpoints, int damage)
+        {
+            HitPoints = hitpoints;
+            Damage = damage;
+        }
+    }
+}
---
+++ Abstraction/Abstraction/SpaceStation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public abstract class SpaceStation
+    {
+        public abstract string FireLaser();
+        public abstract int HitPoints { get; set; }
+        public abstract int Damage { get; set; }
+        
+    }
+}
